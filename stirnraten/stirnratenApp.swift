182d7eca41985781e1e770d4929e848aa2583f7b
leahloevenich <leah.loevenich@gmail.com>
2026-08-05 15:06:58 +0200

    Initial Commit

---
+++ stirnraten/stirnratenApp.swift
@@ -0,0 +1,17 @@
+//
+//  stirnratenApp.swift
+//  stirnraten
+//
+//  Created by Leah Marie Lövenich on 05.08.26.
+//
+
+import SwiftUI
+
+@main
+struct stirnratenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
